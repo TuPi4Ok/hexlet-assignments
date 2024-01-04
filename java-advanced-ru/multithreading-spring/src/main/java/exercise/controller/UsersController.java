8d9551754f95adc96f758c5e7bbf96dfcd702573
@hexlet/cli <support@hexlet.io>
2024-01-04 16:50:41 +0300

    submit java-advanced-ru/multithreading-spring

---
+++ java-advanced-ru/multithreading-spring/src/main/java/exercise/controller/UsersController.java
@@ -0,0 +1,43 @@
+package exercise.controller;
+
+import exercise.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import exercise.service.UserService;
+
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(path = "")
+    public Flux<User> getUsers() {
+        return userService.findAll();
+    }
+
+    // BEGIN
+    @GetMapping("/{id}")
+    public Mono<User> getUser(@PathVariable("id") long id) {
+        return userService.getUser(id);
+    }
+    @PostMapping("")
+    public Mono<User> createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+    @PatchMapping("/{id}")
+    public Mono<User> updateUser(@PathVariable("id") long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+    @DeleteMapping("/{id}")
+    public Mono<User> deleteUser(@PathVariable("id") long id) {
+        return userService.deleteUser(id);
+    }
+    // END
+}
